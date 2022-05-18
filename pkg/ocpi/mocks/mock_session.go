014a687a70dc7b3bca7a2a5925bbf44579c0c3d3
Ross Savage <ross.savage@agrista.com>
2022-05-18 22:00:05 +0100

    Add session and cdr clients to ocpi service

---
+++ pkg/ocpi/mocks/mock_session.go
@@ -0,0 +1,23 @@
+package mocks
+
+import (
+	"context"
+	"errors"
+
+	"github.com/satimoto/go-ocpi-api/ocpirpc"
+	"google.golang.org/grpc"
+)
+
+func (s *MockOcpiService) SessionCreated(ctx context.Context, in *ocpirpc.SessionCreatedRequest, opts ...grpc.CallOption) (*ocpirpc.SessionCreatedResponse, error) {
+	if len(s.sessionCreatedMockData) == 0 {
+		return &ocpirpc.SessionCreatedResponse{}, errors.New("NotFound")
+	}
+
+	response := s.sessionCreatedMockData[0]
+	s.sessionCreatedMockData = s.sessionCreatedMockData[1:]
+	return response, nil
+}
+
+func (s *MockOcpiService) SetSessionCreatedMockData(mockData *ocpirpc.SessionCreatedResponse) {
+	s.sessionCreatedMockData = append(s.sessionCreatedMockData, mockData)
+}
