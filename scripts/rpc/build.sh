a1382f38962cac1ae1994f40f151e3e8a354b0eb
Ross Savage <ross.savage@agrista.com>
2022-06-10 22:12:14 +0200

    Add rpc proto for testing connectivity

--- scripts/rpc/build.sh
+++ scripts/rpc/build.sh
@@ -5,3 +5,4 @@ protoc ocpirpc/command.proto --go_out=plugins=grpc:$GOPATH/src
 protoc ocpirpc/session.proto --go_out=plugins=grpc:$GOPATH/src
 protoc ocpirpc/cdr.proto --go_out=plugins=grpc:$GOPATH/src
 protoc ocpirpc/token.proto --go_out=plugins=grpc:$GOPATH/src
+protoc ocpirpc/rpc.proto --go_out=plugins=grpc:$GOPATH/src
