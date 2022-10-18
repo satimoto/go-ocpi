5c8b5929160414da527c4c1570dff6d6f526c550
Ross Savage <ross.savage@agrista.com>
2022-10-18 15:52:52 +0200

    Update the token authorization if the token type not RFID and session is not yet created

--- internal/rpc/tokenauthorization/resolve.go
+++ internal/rpc/tokenauthorization/resolve.go
@@ -2,6 +2,9 @@ package tokenauthorization
 
 import (
 	"github.com/satimoto/go-datastore/pkg/db"
+	"github.com/satimoto/go-datastore/pkg/session"
+	"github.com/satimoto/go-datastore/pkg/token"
+	"github.com/satimoto/go-datastore/pkg/tokenauthorization"
 	"github.com/satimoto/go-ocpi/internal/async"
 	"github.com/satimoto/go-ocpi/internal/service"
 )
@@ -9,15 +12,21 @@ import (
 type RpcTokenAuthorizationRepository interface{}
 
 type RpcTokenAuthorizationResolver struct {
-	Repository   RpcTokenAuthorizationRepository
-	AsyncService *async.AsyncService
+	Repository                   RpcTokenAuthorizationRepository
+	AsyncService                 *async.AsyncService
+	SessionRepository            session.SessionRepository
+	TokenRepository              token.TokenRepository
+	TokenAuthorizationRepository tokenauthorization.TokenAuthorizationRepository
 }
 
 func NewResolver(repositoryService *db.RepositoryService, services *service.ServiceResolver) *RpcTokenAuthorizationResolver {
 	repo := RpcTokenAuthorizationRepository(repositoryService)
 
 	return &RpcTokenAuthorizationResolver{
-		Repository:   repo,
-		AsyncService: services.AsyncService,
+		Repository:                   repo,
+		AsyncService:                 services.AsyncService,
+		SessionRepository:            session.NewRepository(repositoryService),
+		TokenRepository:              token.NewRepository(repositoryService),
+		TokenAuthorizationRepository: tokenauthorization.NewRepository(repositoryService),
 	}
 }
