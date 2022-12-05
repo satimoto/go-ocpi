44ac21102d84f4de9464d1d78abcead10cf25290
Ross Savage <ross.savage@agrista.com>
2022-12-05 10:41:10 +0100

    Create and update NlConn tariffs

--- internal/sync/nlcon/service.go
+++ internal/sync/nlcon/service.go
@@ -11,7 +11,12 @@ import (
 	"github.com/satimoto/go-datastore/pkg/db"
 	"github.com/satimoto/go-datastore/pkg/param"
 	"github.com/satimoto/go-datastore/pkg/util"
+	"github.com/satimoto/go-ocpi/internal/dto"
 	metrics "github.com/satimoto/go-ocpi/internal/metric"
+)
+
+const (
+	TARIFF_UID_TEMPLATE = "connect.ned-%v"
 )
 
 func (r *NlConService) StartService(shutdownCtx context.Context, waitGroup *sync.WaitGroup) {
@@ -30,13 +35,13 @@ func (r *NlConService) Run(ctx context.Context, credential db.Credential, lastUp
 	r.waitGroup.Add(1)
 	log.Printf("Start NlCon Tariff sync")
 
-	r.updateConnectors(ctx)
+	r.updateConnectors(ctx, credential)
 
 	log.Printf("End NlCon Tariff sync")
 	r.waitGroup.Done()
 }
 
-func (r *NlConService) updateConnectors(ctx context.Context) {
+func (r *NlConService) updateConnectors(ctx context.Context, credential db.Credential) {
 	url := "https://plonweuconspofa01.azurewebsites.net/api/v1/nl/con/tariffs"
 
 	request, err := http.NewRequest(http.MethodGet, url, nil)
@@ -64,10 +69,16 @@ func (r *NlConService) updateConnectors(ctx context.Context) {
 		return
 	}
 
-	for _, nlConTariffDto := range nlConTariffsDto {
-		likeTariffUid := fmt.Sprintf("%%%s", nlConTariffDto.TariffName)
+	tariffMap := make(map[string]db.Tariff)
 
-		if tariff, err := r.TariffRepository.GetTariffLikeUid(ctx, likeTariffUid); err == nil {
+	for _, nlConTariffDto := range nlConTariffsDto {
+		if _, ok := tariffMap[nlConTariffDto.TariffName]; !ok {
+			if tariff, err := r.updateTariff(ctx, credential, nlConTariffDto); err == nil {
+				tariffMap[nlConTariffDto.TariffName] = tariff
+			}
+		}
+
+		if tariff, ok := tariffMap[nlConTariffDto.TariffName]; ok {
 			connectors, err := r.ConnectorRepository.ListConnectorsByEvseID(ctx, util.SqlNullString(nlConTariffDto.EvseID))
 
 			if err != nil {
@@ -91,4 +102,76 @@ func (r *NlConService) updateConnectors(ctx context.Context) {
 			}
 		}
 	}
+}
+
+func (r *NlConService) updateTariff(ctx context.Context, credential db.Credential, nlConTariffDto *NlConTariffDto) (db.Tariff, error) {
+	likeTariffUid := fmt.Sprintf("%%%s", nlConTariffDto.TariffName)
+	tariff, err := r.TariffRepository.GetTariffLikeUid(ctx, likeTariffUid)
+
+	if err != nil {
+		// Create the tariff
+		tariffUid := fmt.Sprintf(TARIFF_UID_TEMPLATE, nlConTariffDto.TariffName)
+
+		tariffParams := db.CreateTariffParams{
+			Uid:                      tariffUid,
+			CredentialID:             credential.ID,
+			Currency:                 "EUR",
+			IsIntermediateCdrCapable: true,
+			LastUpdated:              time.Now().UTC(),
+		}
+
+		tariff, err = r.TariffRepository.CreateTariff(ctx, tariffParams)
+
+		if err != nil {
+			metrics.RecordError("OCPI315", "Error creating tariff", err)
+			log.Printf("OCPI315: Params=%#v", tariffParams)
+			return db.Tariff{}, err
+		}
+	}
+
+	// Construct the elements
+	var elementsDto []*dto.ElementDto
+
+	if nlConTariffDto.StartTariff > 0 {
+		elementDto := dto.ElementDto{}
+		elementDto.PriceComponents = append(elementDto.PriceComponents, &dto.PriceComponentDto{
+			Type:     db.TariffDimensionFLAT,
+			Price:    nlConTariffDto.StartTariff,
+			StepSize: 1,
+		})
+
+		elementsDto = append(elementsDto, &elementDto)
+	}
+
+	if nlConTariffDto.EnergyTariff > 0 {
+		elementDto := dto.ElementDto{}
+		elementDto.PriceComponents = append(elementDto.PriceComponents, &dto.PriceComponentDto{
+			Type:     db.TariffDimensionENERGY,
+			Price:    nlConTariffDto.EnergyTariff,
+			StepSize: 1,
+		})
+
+		elementsDto = append(elementsDto, &elementDto)
+	}
+
+	if nlConTariffDto.ParkingTariff != nil {
+		elementDto := dto.ElementDto{}
+		elementDto.PriceComponents = append(elementDto.PriceComponents, &dto.PriceComponentDto{
+			Type:     db.TariffDimensionTIME,
+			Price:    *nlConTariffDto.ParkingTariff,
+			StepSize: int32(util.DefaultInt(nlConTariffDto.ParkingStepSize, 1)),
+		})
+
+		if nlConTariffDto.ParkingGracePeriodInMinutes != nil {
+			elementDto.Restrictions = &dto.ElementRestrictionDto{
+				MinDuration: nlConTariffDto.ParkingGracePeriodInMinutes,
+			}
+		}
+
+		elementsDto = append(elementsDto, &elementDto)
+	}
+
+	r.ElementResolver.ReplaceElements(ctx, tariff, elementsDto)
+
+	return tariff, nil
 }
