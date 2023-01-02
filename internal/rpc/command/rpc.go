56220bf53a2f61cf9e5fda219413154564186a12
Ross Savage <ross.savage@agrista.com>
2023-01-02 21:05:16 +0100

    Remove verification key

--- internal/rpc/command/rpc.go
+++ internal/rpc/command/rpc.go
@@ -12,7 +12,6 @@ import (
 	metrics "github.com/satimoto/go-ocpi/internal/metric"
 	"github.com/satimoto/go-ocpi/ocpirpc"
 	ocpiCommand "github.com/satimoto/go-ocpi/pkg/ocpi/command"
-	ocpiTokenAuthorization "github.com/satimoto/go-ocpi/pkg/ocpi/tokenauthorization"
 )
 
 func (r *RpcCommandResolver) ReserveNow(ctx context.Context, input *ocpirpc.ReserveNowRequest) (*ocpirpc.ReserveNowResponse, error) {
@@ -130,14 +129,6 @@ func (r *RpcCommandResolver) StartSession(ctx context.Context, input *ocpirpc.St
 			return nil, errors.New("error starting session")
 		}
 
-		verificationKey, err := ocpiTokenAuthorization.CreateVerificationKey(*tokenAuthorization)
-
-		if err != nil {
-			metrics.RecordError("OCPI282", "Error creating verification key", err)
-			log.Printf("OCPI282: TokenAuthorization=%#v", tokenAuthorization)
-			return nil, errors.New("error starting session")
-		}
-
 		command, err := r.CommandResolver.StartSession(ctx, credential, *tokenAuthorization, &input.EvseUid)
 
 		if err != nil {
@@ -146,7 +137,7 @@ func (r *RpcCommandResolver) StartSession(ctx context.Context, input *ocpirpc.St
 			return nil, err
 		}
 
-		startResponse := ocpiCommand.NewCommandStartResponse(*command, verificationKey)
+		startResponse := ocpiCommand.NewCommandStartResponse(*command)
 
 		return startResponse, nil
 	}
