4e6e3ee80fd8108fb50e853a6579fd34cd06cf06
Ross Savage <ross.savage@agrista.com>
2023-01-12 16:19:28 +0100

    Send a notification if there is an issue authorising a token

--- internal/tokenauthorization/v2.1.1/notification.go
+++ internal/tokenauthorization/v2.1.1/notification.go
@@ -9,25 +9,46 @@ import (
 	"github.com/satimoto/go-ocpi/internal/notification"
 )
 
-func (r *TokenAuthorizationResolver) SendNotification(user db.User, authorizationID string) {
+func (r *TokenAuthorizationResolver) SendContentNotification(user db.User, title, body string) {
+	if user.DeviceToken.Valid {
+		message := &fcm.Message{
+			To: user.DeviceToken.String,
+			Notification: &fcm.Notification{
+				Title: title,
+				Body:  body,
+			},
+		}
+
+		_, err := r.NotificationService.SendNotificationWithRetry(message, 10)
+
+		if err != nil {
+			metrics.RecordError("OCPI333", "Error sending notification", err)
+			log.Printf("OCPI333: Message=%v", message)
+		}
+	}
+}
+
+func (r *TokenAuthorizationResolver) SendDataNotification(user db.User, authorizationID string) {
 	dto := notification.CreateTokenAuthorizeNotificationDto(authorizationID)
 
 	r.sendNotification(user, dto)
 }
 
 func (r *TokenAuthorizationResolver) sendNotification(user db.User, data notification.NotificationDto) {
-	message := &fcm.Message{
-		To:               user.DeviceToken.String,
-		ContentAvailable: true,
-		Priority:         "high",
-		Data:             data,
-	}
+	if user.DeviceToken.Valid {
+		message := &fcm.Message{
+			To:               user.DeviceToken.String,
+			ContentAvailable: true,
+			Priority:         "high",
+			Data:             data,
+		}
 
-	_, err := r.NotificationService.SendNotificationWithRetry(message, 10)
+		_, err := r.NotificationService.SendNotificationWithRetry(message, 10)
 
-	if err != nil {
-		// TODO: Cancel session?
-		metrics.RecordError("OCPI286", "Error sending notification", err)
-		log.Printf("OCPI286: Message=%v", message)
+		if err != nil {
+			// TODO: Cancel session?
+			metrics.RecordError("OCPI286", "Error sending notification", err)
+			log.Printf("OCPI286: Message=%v", message)
+		}
 	}
 }
